7d2a227cf9d0c7ed3905efb4c2765c963f380ac1
chrisPickford1 <drchrispickford@gmail.com>
2019-06-09 20:09:22 +0100

    initial project version

---
+++ Src/config.py
@@ -0,0 +1,88 @@
+from datetime import datetime, timedelta
+import os
+import logging
+import sys
+
+OS = 'Server'
+
+if OS == 'Server':
+    sys.path.append(os.path.join(os.path.expanduser('G:'), os.path.sep, 'PYTHON','Datascience Packages'))
+elif OS == 'Local':
+    sys.path.append(os.path.join(os.path.expanduser('~'), 'Documents', 'Coding', 'Python', 'PyCharm', 'Projects'))
+
+from UtilityPackagesV2.config import config
+
+class credentials(object):
+    def __init__(self):
+        self.username = 'datascience'
+        self.password = os.environ.get('datascienceAccount')
+
+class privateConfig(config):
+    def __init__(self, OS, PROJECT_NAME):
+        config.__init__(self)
+
+
+        if OS == 'Local->Server':
+            self.ROOT = os.path.join(os.path.expanduser('/Volumes/g$'), 'Chris Pickford', 'Coding', 'Python', 'Projects')
+            self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
+            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('/Volumes/g$'), 'PYTHON','Datascience Packages')
+        elif OS == 'Local':
+            self.ROOT = os.path.join(os.path.expanduser('~'),'Documents', 'Coding','Python', 'Projects')
+            self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
+            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('~'),'Documents', 'Coding','Python', 'Projects')
+        elif OS == 'Server':
+            self.ROOT = os.path.join(os.path.expanduser('D:'), os.path.sep, 'Chris Pickford', 'Projects')
+            self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
+            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('D:'), os.path.sep, 'Datascience Packages')
+
+        # Add the project root and package roots to the path
+        sys.path.append(self.ROOT)
+        sys.path.append(self.PROJECT_ROOT)
+        sys.path.append(self.PACKAGE_ROOT)  ## Project specific imports
+
+        from CodeCandy.haribo import jupyter as candy
+
+        candy.help()
+        candy.set_screen_width()## Jupyter Environment setup
+
+
+
+
+
+    brands = {
+        '105580120': {
+            'Market': 'UK', 'Account': 'Android App'}
+        , '87279108': {
+            'Market': 'UK', 'Account': 'Bonvoyage'}
+        , '90231540': {
+            'Market': 'UK', 'Account': 'OTB'}
+        , '6873450': {
+            'Market': 'UK', 'Account': 'Sunshine'}
+        , '89491622': {
+            'Market': 'UK', 'Account': 'iOS App'}
+        , '178065175': {
+            'Market': 'DK', 'Account': 'eBeach.dk'}
+        , '34390255': {
+            'Market': 'NO', 'Account': 'eBeach.no'}
+        , '1000308386': {
+            'Market': 'SE', 'Account': 'eBeach.se'}
+        , '127798343': {
+            'Market': 'SE', 'Account': 'iOS App'}
+    }
+
+    statusLog = {
+        'OK ': '[__OK__]',
+        'FAIL': '[_FAIL_]'
+    }
+    format = '%(asctime)s %(status)-8s %(separator)-3s %(message)s'
+
+    dctLog = {
+        'FAIL': {
+            'status': '[ FAIL ]',
+            'separator': ' - '
+        },
+        'OK': {
+            'status': '[      ]',
+            'separator': ' - '
+        }
+    }
---
+++ Src/main.py
@@ -0,0 +1,63 @@
+"""
+
+
+Naming Conventions:
+
+Package:            thispackage (short name)
+Module:             this_module (short name)
+Class:              ThisIsAClass
+Function:           this_is_a_function
+Public Method:      this_is_a_public_method
+Non-Public Method:  _this_is_a_non_public_method
+Variables:          thisIsAVariable
+Constant:           THIS_IS_A_CONSTANT
+
+"""
+
+__status__  = "development"
+__version__ = 'mdl 1.0.1'
+__date__    = "DATE"
+__author__  = 'AUTHOR'
+
+'''
+Ensure any virtual environments have been activated before running scripts
+'''
+import os
+import sys
+import config as cfg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+cwd = os.getcwd()
+print('Working directory set as:', cwd)
+
+PROJECT_NAME = 'PROJECT NAME'
+config = cfg.privateConfig(cfg.OS, PROJECT_NAME)
+newUser = cfg.credentials()
+logFileName = PROJECT_NAME +'.log'
+_ = config.set_logging(logFileName)
+
+
+rootPath = config.ROOT
+sys.path.append(os.path.join(rootPath))
+sys.path.append(os.path.join(config.PACKAGE_ROOT, 'UtilityPackagesV2'))
+
+config.abort_pass_programme(logString='-------Program Started-------')
+
+from UtilityPackagesV2.dbutility import gnrl_database_interaction as gdbi
+from UtilityPackagesV2.dbutility import credentials
+from UtilityPackagesV2.mt_email import email_
+config.abort_pass_programme(logString=' Imported UtilityPackages ')
+
+##################################################################################################################
+
+
+
+def main():
+    pass
+
+
+
+
+
+
+
