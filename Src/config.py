e4c0c1357e1a501f541f400e66491320e97b169d
chrisPickford1 <drchrispickford@gmail.com>
2019-06-09 21:19:30 +0100

    Update the config file for generic use

--- Src/config.py
+++ Src/config.py
@@ -6,7 +6,7 @@ import sys
 OS = 'Server'
 
 if OS == 'Server':
-    sys.path.append(os.path.join(os.path.expanduser('G:'), os.path.sep, 'PYTHON','Datascience Packages'))
+    sys.path.append(os.path.join(os.path.expanduser('~:'), os.path.sep, 'PYTHON','Datascience Packages'))
 elif OS == 'Local':
     sys.path.append(os.path.join(os.path.expanduser('~'), 'Documents', 'Coding', 'Python', 'PyCharm', 'Projects'))
 
@@ -22,18 +22,14 @@ class privateConfig(config):
         config.__init__(self)
 
 
-        if OS == 'Local->Server':
-            self.ROOT = os.path.join(os.path.expanduser('/Volumes/g$'), 'Chris Pickford', 'Coding', 'Python', 'Projects')
-            self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
-            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('/Volumes/g$'), 'PYTHON','Datascience Packages')
-        elif OS == 'Local':
+        if OS == 'Local':
             self.ROOT = os.path.join(os.path.expanduser('~'),'Documents', 'Coding','Python', 'Projects')
             self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
             self.PACKAGE_ROOT = os.path.join(os.path.expanduser('~'),'Documents', 'Coding','Python', 'Projects')
         elif OS == 'Server':
-            self.ROOT = os.path.join(os.path.expanduser('D:'), os.path.sep, 'Chris Pickford', 'Projects')
+            self.ROOT = os.path.join(os.path.expanduser('~:'), os.path.sep, 'Chris Pickford', 'Projects')
             self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
-            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('D:'), os.path.sep, 'Datascience Packages')
+            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('~:'), os.path.sep, 'Datascience Packages')
 
         # Add the project root and package roots to the path
         sys.path.append(self.ROOT)
@@ -45,30 +41,6 @@ class privateConfig(config):
         candy.help()
         candy.set_screen_width()## Jupyter Environment setup
 
-
-
-
-
-    brands = {
-        '105580120': {
-            'Market': 'UK', 'Account': 'Android App'}
-        , '87279108': {
-            'Market': 'UK', 'Account': 'Bonvoyage'}
-        , '90231540': {
-            'Market': 'UK', 'Account': 'OTB'}
-        , '6873450': {
-            'Market': 'UK', 'Account': 'Sunshine'}
-        , '89491622': {
-            'Market': 'UK', 'Account': 'iOS App'}
-        , '178065175': {
-            'Market': 'DK', 'Account': 'eBeach.dk'}
-        , '34390255': {
-            'Market': 'NO', 'Account': 'eBeach.no'}
-        , '1000308386': {
-            'Market': 'SE', 'Account': 'eBeach.se'}
-        , '127798343': {
-            'Market': 'SE', 'Account': 'iOS App'}
-    }
 
     statusLog = {
         'OK ': '[__OK__]',
