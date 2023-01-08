d2ff43723ab6fee7d4db1142996574dbab4682ab
chrisPickford1 <drchrispickford@gmail.com>
2023-01-08 21:17:04 +0000

    fixed merge conflicts

--- Src/config.py
+++ Src/config.py
@@ -3,47 +3,65 @@ import os
 import logging
 import sys
 
-OS = 'Local'
+OS = "Local"
 
-if OS == 'Server':
-    sys.path.append(os.path.join(os.path.expanduser('~:'), os.path.sep, 'PYTHON','Datascience Packages'))
-elif OS == 'Local':
-    sys.path.append(os.path.join(os.path.expanduser('~'), 'Documents', 'Coding', 'Python','Projects'))
+if OS == "Server":
+    sys.path.append(
+        os.path.join(
+            os.path.expanduser("~:"), os.path.sep, "PYTHON", "Datascience Packages"
+        )
+    )
+elif OS == "Local":
+    sys.path.append(
+        os.path.join(
+            os.path.expanduser("~"), "Documents", "Coding", "Python", "Projects"
+        )
+    )
 
 from DataCrane.config import config
 
-config.googleServiceAccountPrivateKey = os.path.join(os.path.expanduser('~')
-                                                          , os.path.sep
-                                                          , 'Users'
-                                                          , 'chrispickford'
-                                                          , 'Documents'
-                                                          , 'Coding'
-                                                          , 'Python'
-                                                          , 'Datascience'
-                                                          , 'PRIVATE KEYS'
-                                                          , 'Google'
-                                                          , 'Datascience Service Account'
-                                                          , 'privateKey.json'
-                                                          )
+config.googleServiceAccountPrivateKey = os.path.join(
+    os.path.expanduser("~"),
+    os.path.sep,
+    "Users",
+    "chrispickford",
+    "Documents",
+    "Coding",
+    "Python",
+    "Datascience",
+    "PRIVATE KEYS",
+    "Google",
+    "Datascience Service Account",
+    "privateKey.json",
+)
+
 
 class credentials(object):
     def __init__(self):
-        self.username = 'datascience'
-        self.password = os.environ.get('datascienceAccount')
+        self.username = "datascience"
+        self.password = os.environ.get("datascienceAccount")
+
 
 class privateConfig(config):
     def __init__(self, OS, PROJECT_NAME):
         config.__init__(self)
 
-
-        if OS == 'Local':
-            self.ROOT = os.path.join(os.path.expanduser('~'),'Documents', 'Coding','Python', 'Projects')
+        if OS == "Local":
+            self.ROOT = os.path.join(
+                os.path.expanduser("~"), "Documents", "Coding", "Python", "Projects"
+            )
             self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
-            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('~'),'Documents', 'Coding','Python', 'Projects')
-        elif OS == 'Server':
-            self.ROOT = os.path.join(os.path.expanduser('~:'), os.path.sep, 'Chris Pickford', 'Projects')
+            self.PACKAGE_ROOT = os.path.join(
+                os.path.expanduser("~"), "Documents", "Coding", "Python", "Projects"
+            )
+        elif OS == "Server":
+            self.ROOT = os.path.join(
+                os.path.expanduser("~:"), os.path.sep, "Chris Pickford", "Projects"
+            )
             self.PROJECT_ROOT = os.path.join(self.ROOT, PROJECT_NAME)
-            self.PACKAGE_ROOT = os.path.join(os.path.expanduser('~:'), os.path.sep, 'Datascience Packages')
+            self.PACKAGE_ROOT = os.path.join(
+                os.path.expanduser("~:"), os.path.sep, "Datascience Packages"
+            )
 
         # Add the project root and package roots to the path
         sys.path.append(self.ROOT)
@@ -53,22 +71,12 @@ class privateConfig(config):
         from CodeCandy.haribo import jupyter as candy
 
         candy.help()
-        candy.set_screen_width()## Jupyter Environment setup
+        candy.set_screen_width()  ## Jupyter Environment setup
 
-
-    statusLog = {
-        'OK ': '[__OK__]',
-        'FAIL': '[_FAIL_]'
-    }
-    format = '%(asctime)s %(status)-8s %(separator)-3s %(message)s'
+    statusLog = {"OK ": "[__OK__]", "FAIL": "[_FAIL_]"}
+    format = "%(asctime)s %(status)-8s %(separator)-3s %(message)s"
 
     dctLog = {
-        'FAIL': {
-            'status': '[ FAIL ]',
-            'separator': ' - '
-        },
-        'OK': {
-            'status': '[      ]',
-            'separator': ' - '
-        }
+        "FAIL": {"status": "[ FAIL ]", "separator": " - "},
+        "OK": {"status": "[      ]", "separator": " - "},
     }
